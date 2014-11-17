e2e88af62a02f5537836c0881f2842f1be86f1d4
AkermanGreg <gregoryakerman@gmail.com>
2014-11-17 15:08:46 -0800

    edit user view and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,11 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-def create
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
     @user = User.new(user_params)
     if @user.save
         session[:user_id] = @user.id
@@ -16,7 +20,11 @@ def create
     else
         render "new"
     end
-end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
 
   private
   def user_params
