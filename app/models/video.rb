38f59faee27c4336614287124066b1ace441d340
sarchome <sarcho@gmail.com>
2014-11-19 19:17:40 -0800

    test

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,11 +1,6 @@
 class Video < ActiveRecord::Base
   attr_accessible :date_submitted, :filepath
 
-  # belongs_to :user
-  # belongs_to :question
-  # has_many :comments
-  # has_many :hashtags
-
   belongs_to :user
   belongs_to :question
   has_many :videos
