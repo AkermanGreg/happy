1411c886be6c7cd62bf051cace2efbb77cb9490f
AkermanGreg <gregoryakerman@gmail.com>
2014-11-20 18:47:02 -0800

    full crud and relationship logic in views on questions / users

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,6 +9,7 @@ class QuestionsController < ApplicationController
 
   def show
     @question = Question.find(params[:id])
+    
   end
 
   def create
@@ -18,6 +19,25 @@ class QuestionsController < ApplicationController
     else
       render "new"
     end
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+      @question = Question.find(params[:id])
+      if @question.update_attributes(question_params)
+        redirect_to questions_path
+      else
+        render 'edit'
+      end
+    end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to questions_path
   end
 
  private
