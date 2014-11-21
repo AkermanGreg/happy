2638326e3b16aa967f040de66ecbeddce05f53f3
sarchome <sarcho@gmail.com>
2014-11-20 16:26:14 -0800

    added crud for videos controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,29 @@
 class VideosController < ApplicationController
 
-  
+  def index
+    @videos = Video.all
+  end
+
+  def new
+    @video = Video.new
+  end
+
+  def show
+    @video = Video.find(params[:id])
+  end
+
+  def create
+    @video = Video.new(video_params)
+    if @video.save
+      redirect_to questions_path
+    else
+      render "new"
+    end
+  end
+
+ private
+  def video_params
+    params.require(:video).permit(:filepath, :user_id)
+  end
+end
 end
