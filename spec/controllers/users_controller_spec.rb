a37b74c47453d00ebaedcbfaf9629bc7c3c297f6
AkermanGreg <gregoryakerman@gmail.com>
2014-12-01 16:27:52 -0800

    style updates

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,5 +2,23 @@ require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
 
+  it "should get page for index" do
 
-end
+    get :index
+
+    expect(response).to be_success
+    expect(response).to have_http_status(200)
+
+  end
+
+  it "should get page for show" do
+
+    get :show
+
+    expect(response).to be_success
+    expect(response).to have_http_status(200)
+
+  end
+
+
+ end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
 
+  before do
+    @user = User.new(username: "Example User", email: "user@example.com",
+                    password: "foobar", password_confirmation: "foobar")
+ 
+
+  subject { @user }
+
   it {should validate_presence_of(:username)}
   it {should respond_to(:email)}
   it {should respond_to(:password)}
